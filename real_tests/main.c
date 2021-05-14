17c7570b93e416740093841045a5afdc83672d95
tohsumi <tohsumi@student.42tokyo.jp>
2021-05-15 03:21:25 +0900

    main.c

--- real_tests/main.c
+++ real_tests/main.c
@@ -6,7 +6,7 @@
 /*   By: dainoue <dainoue@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 22:22:21 by tohsumi           #+#    #+#             */
-/*   Updated: 2021/05/15 02:32:57 by tohsumi          ###   ########.fr       */
+/*   Updated: 2021/05/15 03:20:20 by tohsumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 int	main(void)
 {
-	if (atoi_launcher()
-		& isalnum_launcher()
-		& isalnum_launcher()
-		& isascii_launcher()
-		& isdigit_launcher()
-		& isprint_launcher()
-		& strchr_launcher()
-		& strlen_launcher()
-		& strrchr_launcher()
-		& tolower_launcher()
-		& toupper_launcher()
-		& strdup_launcher()
-		& memcmp_launcher()
-		& strnstr_launcher()
-		& strncmp_launcher())
-		return (-1);
-	else
-		return (0);
+	atoi_launcher();
+	isalnum_launcher();
+	isalnum_launcher();
+	isascii_launcher();
+	isdigit_launcher();
+	isprint_launcher();
+	strchr_launcher();
+	strlen_launcher();
+	strrchr_launcher();
+	tolower_launcher();
+	toupper_launcher();
+	strdup_launcher();
+	memcmp_launcher();
+	strnstr_launcher();
+	strncmp_launcher();
+	return (0);
 }
